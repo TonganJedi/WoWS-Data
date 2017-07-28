e02d0e70bc66b6f96122648cc5e13ee68b7bb062
TonganJedi <tonganjedi@gmail.com>
2017-07-28 15:21:23 -0600

    added basic components, routing, and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './error/pageNotFound.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: false } // <-- debugging purposes only
+    ),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/error/pageNotFound.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pageNotFound',
+    templateUrl: 'pageNotFound.component.html',
+    styleUrls: ['pageNotFound.component.css']
+})
+
+export class PageNotFoundComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
